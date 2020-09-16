d7d90fbfb8d31443b9b2525518ad2d9e61e29cd3
aimkiray <root@meowwoo.com>
2020-09-16 16:18:56 +0800

    fix: executable

--- daemon/build-daemon.sh
+++ daemon/build-daemon.sh
@@ -2,7 +2,8 @@
 
 set -eo pipefail
 
-img_name=lotus-daemon
+target=daemon
+img_name=lotus-$target
 lotus_version=v0.7.0
 
 export proxy=http://127.0.0.1:8000
@@ -14,4 +15,5 @@ docker build \
     -t aimkiray/$img_name:$lotus_version \
     -t aimkiray/$img_name:latest \
     --network host \
+    -f $target.dockerfile \
     .
