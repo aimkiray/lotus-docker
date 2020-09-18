f5749d3bf5680387646849d5e2f2cfc17e41aea4
aimkiray <root@meowwoo.com>
2020-09-18 11:10:00 +0800

    fix: run script

--- worker/run-worker-prod.sh
+++ worker/run-worker-prod.sh
@@ -5,10 +5,7 @@ run_dir=/data2/lotus/$img_name
 
 docker container rm $img_name -f
 
-host_ip=$(ip route get 255.255.255.255 | grep -Po '(?<=src )(\d{1,3}.){4}' | xargs)
-
-
-mv -f config.toml $run_dir/config.toml
+mkdir -p $run_dir
 
 # 注意，JWT_TOKEN 在 miner 数据目录下的 token 文件
 # MINER_IP 是 miner 所在主机的内网 IP
